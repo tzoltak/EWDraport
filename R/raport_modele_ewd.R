88514778e4101c02b74029e85d10f16be078a708
tzoltak <tomek@zozlak.org>
2018-10-13 19:51:27 +0200

    Przejścia na kable() w tworzeniu tabel.

--- R/raport_modele_ewd.R
+++ R/raport_modele_ewd.R
@@ -2,13 +2,53 @@
 #' @description
 #' Funkcja przygotowuje nieco przekazaną jej macierz lub ramkę danych,
 #' a następnie zamienia ją na kod Rmarkdown przy pomocy funkcji.
+#' \code{\link[knitr]{kable}}
+#' @param tab ramka danych lub
+#' @param naglowek treść nagłówka
+#' @param format.args opcjonalnie list dodatkowych argumentów opisujących
+#' formatowanie liczb w tabeli (p. \code{\link[knitr]{kable}})
+#' @return
+#' Funkcja zwraca kod Rmarkdown, zwrócony przez funkcję
+#' \code{\link[knitr]{kable}}.
+#' @importFrom knitr kable
+#' @export
+kable_table = function(tab, naglowek = NULL,
+                       format.args = list(decimal.mark = ",", big.mark = " ")){
+  tab = as.data.frame(tab)
+  rownames(tab) = NULL
+  names(tab) = enc2native(names(tab))
+  names(tab) = sub("plec", "płeć", names(tab))
+
+  for (i in 1:ncol(tab)) {
+    if (all(grepl("TRUE|FALSE", as.character(tab[, i])))) {
+      tab[, i] = as.factor(ifelse(grepl("TRUE", tab[, i]), "tak", "nie"))
+    }
+  }
+  classCol = sapply(tab, class)
+  align = ifelse(classCol %in% c("numeric", "integer"), "r", "l")
+  tab[, classCol == "numeric"] = round(tab[, classCol == "numeric"], 4)
+  for (i in which(classCol == "numeric")) {
+    # brutalne sprawdzanie dokładności zapisu
+    for (j in 0:4) {
+      if (all(tab[, i] == round(tab[, i] * 10^j, 0) / 10^j)) {
+        break
+      }
+    }
+    tab[, i] = format(tab[, i], nsmall = j, decimal.mark = ",", big.mark = " ")
+  }
+  kable(tab, align = align, caption = naglowek, format.args = format.args)
+}
+#' @title Drukowanie tabeli
+#' @description
+#' Funkcja przygotowuje nieco przekazaną jej macierz lub ramkę danych,
+#' a następnie zamienia ją na kod Rmarkdown przy pomocy funkcji.
 #' \code{\link[pander]{pander}}
 #' @param tab ramka danych lub macierz
 #' @param ... opcjonalnie dodatkowe argumenty, które mają zostać przekazane
 #' funkcji \code{\link[pander]{pander}}
 #' @return
-#' Funkcja zwraca kod Rmarkdown, zwrócony przez funkcję
-#' \code{\link[pander]{pander}}
+#' Funkcja nic nie zwraca, ale jej wywołanie skutkuje przekazaniem na stdout
+#' kodu Rmarkdown wygenerowanego przez funkcję \code{\link[pander]{pander}}.
 #' @importFrom pander pander
 #' @export
 pander_table = function(tab, ...){
@@ -35,7 +75,7 @@ pander_table = function(tab, ...){
     tab[, i] = format(tab[, i], nsmall = j)
   }
 
-  pander(tab, justify = just,  ...);
+  pander(tab, justify = just,  ...)
 }
 #' @title Naglowek tabeli
 #' @description
@@ -48,8 +88,12 @@ pander_table = function(tab, ...){
 #' \code{\link[pander]{set.caption}}
 #' @importFrom pander set.caption
 #' @export
-ustaw_naglowek_tabeli = function(tekst, tabelaStr = "Tabela"){
-  set.caption(paste0(numeracja(tabelaStr), " ", tekst))
+ustaw_naglowek_tabeli = function(tekst, tabelaStr = "Tabela", tryb = "kable"){
+  if (tryb == "pander") {
+    set.caption(paste0(numeracja(tabelaStr), " ", tekst))
+  } else if (tryb == "kable") {
+    return(paste0(numeracja(tabelaStr), " ", tekst))
+  }
 }
 #' @title Naglowek wykresu
 #' @description
